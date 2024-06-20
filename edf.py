f2d45e500b51a35abf0c092b9da7e88d1ac0141e
Ltttttt2000 <msliutong@gmail.com>
2024-06-20 16:20:09 +0800

    620

--- edf.py
+++ edf.py
@@ -1,15 +1,17 @@
 import json
 import mne
 
-data_path = 'D:\\WangYinyanRaw\\术中唤醒\\ECoG_20231030_A_P_1\\ECoG_20231030_A_P_1_Movments_Session_1_datRaw.edf'   # 存放数据的具体位置，需要改成自己数据存放的地方
+# data_path = 'D:\\WangYinyanRaw\\术中唤醒\\ECoG_20231030_A_P_1\\ECoG_20231030_A_P_1_Movments_Session_1_datRaw.edf'   # 存放数据的具体位置，需要改成自己数据存放的地方
 # data_path = 'D:\\WangYinyanRaw\\短期植入\\20231217 Shandong\\Lishilong_20231215_motor1\\Lishilong_20231215_motor1.edf'
-
+data_path = 'D:\\WangYinyanRaw\\短期植入\\20240223 Shandong2\\YangShengQiang_20240223_1_Left_Big\\YangShengQiang_20240223_1_Left_Big.edf'
 
 # signal_name  = 'EEG Fpz-Cz'     # 所选的通道名称
 raw_data = mne.io.read_raw_edf(data_path, preload=False)
 print(raw_data)  # 32 x 244500
 
 
+print(raw_data.ch_names)
+print(raw_data.n_times)
 # 查看原始edf文件中保存的event id以及events
 # events_from_annot, event_dict = mne.events_from_annotations(raw_data)
 # print(event_dict)
--- nsx.py
+++ nsx.py
@@ -2,9 +2,11 @@ import mne
 
 path = 'D:\\柔性电极动物实验数据\\大鼠64通道\\柔性电极-64通道_神经信号_第50天E.ns5'
 # ns = mne.io.read_raw_nsx(path)
-ns = mne.io.read_raw_nsx('D:\\柔性电极动物实验数据\\猕猴急性记录\\dataset1\\datafile002.ns6')
+ns = mne.io.read_raw_nsx('D:\\柔性电极动物实验数据\\猕猴急性记录\\20211126_AP_monkey_3\\neural\\dataset003.ns3')
 print(type(ns))
 print('ns.ch_names:', ns.ch_names)
 # print(ns.n_times)
 print(ns)
 
+# ns6: 125936401
+# ns3: 8395760  2khz
